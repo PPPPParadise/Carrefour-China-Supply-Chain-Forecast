117790f3640a89d9161b6606f1832f92e7382579
House Gong <house.gong@artefact.com>
2019-08-10 18:03:18 +0800

    Add simulation process for airflow

---
+++ demand_planning/source_code/Carrefour_Forecast/src/main/scala/carrefour/forecast/process/SimulationProcess.scala
@@ -0,0 +1,103 @@
+package carrefour.forecast.process
+
+import carrefour.forecast.core.ProcessLogic
+import carrefour.forecast.model.EnumFlowType.FlowType
+import carrefour.forecast.model.ModelRun
+
+/**
+  * Simulation process
+  * 订单模拟运行
+  */
+object SimulationProcess {
+
+  def main(args: Array[String]): Unit = {
+    val runDate = args(0)
+    val flowType = FlowType.DC
+    val defaultStockLevel = 0.0
+    val defaultDeliveryTime = "AfterStoreOpen"
+
+    var item_id = 0
+    var sub_id = 0
+    var store_code = ""
+    var isDebug = false
+    var debugTable = ""
+
+    for (i <- 1 until args.length) {
+      val argPair = args(i).split("=")
+
+      if (argPair(0).equalsIgnoreCase("item_id")) {
+        item_id = Integer.valueOf(argPair(1))
+      }
+
+      if (argPair(0).equalsIgnoreCase("sub_id")) {
+        sub_id = Integer.valueOf(argPair(1))
+      }
+
+      if (argPair(0).equalsIgnoreCase("store_code")) {
+        store_code = argPair(1)
+      }
+
+      if (argPair(0).equalsIgnoreCase("debug_table")) {
+        isDebug = true
+        debugTable = argPair(1)
+      }
+
+      if (argPair(0).equalsIgnoreCase("help")) {
+        throw new IllegalArgumentException("Sample input:  item_id=6690 sub_id=5324 " +
+          "con_holding=101 debugTable=vartefact.forecast_debug")
+      }
+    }
+
+    val onstockRun = ModelRun(runDate,
+      flowType,
+      "vartefact.forecast_onstock_orders",
+      "vartefact.forecast_onstock_orders_hist",
+      "tmp_in_scope_onstock_item_store",
+      defaultStockLevel,
+      defaultDeliveryTime,
+      item_id,
+      sub_id,
+      store_code,
+      false,
+      false,
+      "",
+      true)
+
+    ProcessLogic.process(onstockRun)
+
+    val xdockRun = ModelRun(runDate,
+      flowType,
+      "vartefact.forecast_xdock_orders",
+      "vartefact.forecast_xdock_orders_hist",
+      "tmp_in_scope_xdock_item_store",
+      defaultStockLevel,
+      defaultDeliveryTime,
+      item_id,
+      sub_id,
+      store_code,
+      false,
+      false,
+      "",
+      true)
+
+    ProcessLogic.process(xdockRun)
+
+    val dcRun = ModelRun(runDate,
+      flowType,
+      "vartefact.forecast_dc_orders",
+      "vartefact.forecast_dc_orders_hist",
+      "tmp_in_scope_dc_item_store",
+      defaultStockLevel,
+      defaultDeliveryTime,
+      item_id,
+      sub_id,
+      "",
+      true,
+      false,
+      "",
+      true)
+
+    ProcessLogic.process(dcRun)
+  }
+
+}
